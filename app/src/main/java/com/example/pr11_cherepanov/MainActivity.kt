7bf846ff17428211c72c7313c5b58a62479dc8cb
Vankst <m.cher3panov@gmail.com>
2022-06-17 23:17:17 +0700

    end pr11

---
+++ app/src/main/java/com/example/pr11_cherepanov/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pr11_cherepanov
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onCheckboxClicked(view: View) {
+        // Получаем Функция перехода на другой экран:
+        val checkBox = view as CheckBox
+        // Получаем, отмечен ли данный флажок
+        if (checkBox.isChecked) {
+        }
+    }
+    fun onClickStepsActivity(view: View){
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/pr11_cherepanov/MainActivity2.kt
@@ -0,0 +1,19 @@
+package com.example.pr11_cherepanov
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlin.reflect.KClass
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+    fun onClickStepsActivity(view: View){
+        val intent = Intent(this, MainActivity3::class.java)
+        startActivity(intent)
+    }
+
+}
